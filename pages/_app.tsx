5c8d7c46d047c94d1851ea65cc87863e6e1bda94
skullcutter02 <=>
2022-01-25 17:39:00 +0800

    Init

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Head from "next/head";
+
+import "../styles/global.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <title>My Website</title>
+        </Head>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
